f7c75fec045b3c024c095e38a6e2f00b73c94a18
david.burkhart <burkhart.david2@synthes.com>
2015-08-21 19:00:53 +0200

    -removed BACKSLASH environment variable +added the app/lib folder to the include path +error handler +NotORM +comments

--- app/index.php
+++ app/index.php
@@ -12,17 +12,28 @@ if (!defined('CONFIG_PATH')) {
 	define('CONFIG_PATH', APPLICATION_PATH.'/config/application.json');
 }
 
-if (!defined('BACKSLASH')) {
-	define('BACKSLASH', '\\');
-}
+set_include_path(implode(PATH_SEPARATOR, array(
+	realpath(APPLICATION_PATH.'/lib'),
+	get_include_path(),
+)));
 
 
+// include all files in the lib folder
 foreach (glob('lib/*.php') as $filename) {
 	include $filename;
 }
 
+// initialize autoloader
 require_once 'Autoloader.php';
 spl_autoload_register(array('Autoloader', 'load'));
 
+// initialize error handler
+require_once 'ErrorHandler.php';
+set_error_handler('ErrorHandler::error');
+set_exception_handler('ErrorHandler::exception');
 
+// include NotORM
+require_once 'notorm/NotORM.php';
+
+// start application
 new controller\Index();
