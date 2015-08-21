4a5cae87d78b570b04b7be4869cb618f0d557802
audacus <david.burkhart@hotmail.com>
2015-08-21 19:32:40 +0200

    ~removed stylesheet

--- app/view/scripts/header.php
+++ app/view/scripts/header.php
@@ -4,7 +4,6 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title><?php echo $this->config->get('app.name'); ?></title>
-	<link rel="stylesheet" href="">
 </head>
 <body>
 <h1>header</h1>
