ec38bbeacea03dee7ec08285f7c8dcafecbd0ce1
audacus <david.burkhart@hotmail.com>
2015-09-07 18:04:18 +0200

    ~temporarily disable error handler due to work in progress +boostrap file for bootstrapping stuff

--- app/index.php
+++ app/index.php
@@ -14,7 +14,7 @@ if (!defined('CONFIG_PATH')) {
 
 set_include_path(implode(PATH_SEPARATOR, array(
 	realpath(APPLICATION_PATH.'/lib'),
-	get_include_path(),
+	get_include_path()
 )));
 
 
@@ -27,10 +27,10 @@ foreach (glob('lib/*.php') as $filename) {
 require_once 'Autoloader.php';
 spl_autoload_register(array('Autoloader', 'load'));
 
-// initialize error handler
-require_once 'ErrorHandler.php';
-set_error_handler('ErrorHandler::error');
-set_exception_handler('ErrorHandler::exception');
+// // initialize error handler
+// require_once 'ErrorHandler.php';
+// set_error_handler('ErrorHandler::error');
+// set_exception_handler('ErrorHandler::exception');
 
 // include NotORM
 require_once 'notorm/NotORM.php';
