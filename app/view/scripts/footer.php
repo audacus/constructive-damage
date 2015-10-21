ca43a6bf642162e42f146416eda6407ab662d434
audacus <david.burkhart@hotmail.com>
2015-10-21 14:42:25 +0200

    ~read the copyright information from the config

--- app/view/scripts/footer.php
+++ app/view/scripts/footer.php
@@ -1,9 +1,16 @@
 </div>
 <div class="footer">
 	<span class="copyright"><?php
-	$startYear = DateTime::createFromFormat('Y-m-d', '2015-08-13')->format('Y');
-	$currentYear = (new DateTime())->format('Y');
-	echo 'Copyright &copy; Hug Studios '.($currentYear <= $startYear ? $currentYear : $startYear.'-'.$currentYear);
+	$author = $this->config->get('app.info.author');
+	$date = $this->config->get('app.info.date');
+	if (!empty($author)) {
+		echo 'Copyright &copy; '.$author;
+		if (!empty($date)) {
+			$startYear = DateTime::createFromFormat('Y-m-d', $date)->format('Y');
+			$currentYear = (new DateTime())->format('Y');
+			echo ' '.($currentYear <= $startYear ? $currentYear : $startYear.'-'.$currentYear);
+		}
+	}
 ?></span>
 </div>
 </div>
