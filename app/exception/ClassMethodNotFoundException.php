7883e38f0977072e52152baca68b9f4b4551b470
audacus <david.burkhart@hotmail.com>
2015-11-04 12:08:56 +0100

    -removed use clause

--- app/exception/ClassMethodNotFoundException.php
+++ app/exception/ClassMethodNotFoundException.php
@@ -2,11 +2,10 @@
 
 namespace exception;
 
-use \Exception;
 
 class ClassMethodNotFoundException extends Exception {
 
-	public function __construct($class, $method, $code = 0, Exception $previous = null) {
+	public function __construct($class, $method, $code = 0, \Exception $previous = null) {
 		parent::__construct("Class method '".get_class($class).'::'.$method.'()'."' could not be found", $code, $previous);
 	}
 }
--- app/exception/ControllerNotFoundException.php
+++ app/exception/ControllerNotFoundException.php
@@ -2,11 +2,9 @@
 
 namespace exception;
 
-use \Exception;
-
 class ControllerNotFoundException extends Exception {
 
-	public function __construct($message, $code = 0, Exception $previous = null) {
+	public function __construct($message, $code = 0, \Exception $previous = null) {
 		parent::__construct("Controller '".$message."' could not be found", $code, $previous);
 	}
 }
